4233bdf98e66729457550bfd59b1214de4bb819c
Taylor Jones <taylor@joneses.net>
2016-12-18 19:49:53 -0500

    Reformatted server.js

--- server.js
+++ server.js
@@ -2,19 +2,8 @@ var express = require("express");
 var app = express();
 var port = 3000;
 var hits = 0;
+var middleware = require('./middleware.js');
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        hits += 1;
-        console.log("Private route hit " + hits + " times.");
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log("Request: " + req.method + " " + req.originalUrl);
-        console.log("Site hit on: " + new Date());
-        next()
-    }
-};
 
 //app.use(middleware.requireAuthentication);
 
