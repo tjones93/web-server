2e80a595ed6f84af2a83142f4aa514eed808af40
Taylor Jones <taylor@joneses.net>
2016-12-18 19:39:31 -0500

    Added ! in server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var middleware = {
 app.use(middleware.logger)
 
 app.get("/about", middleware.requireAuthentication, function (req, res) {
-    res.send("About me");
+    res.send("About me!!!!!!!!");
 });
 
 app.use(express.static(__dirname + "/public"))
